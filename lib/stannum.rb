9224776cff71b70577cedc1930877ceb076f341f
Rob Smith <merlin@sleepingkingstudios.com>
2021-02-19 14:52:24 -0500

    Add helper class methods to Stannum.

--- lib/stannum.rb
+++ lib/stannum.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'stannum/version'
+
 # A library for specifying and validating data structures.
 module Stannum
   autoload :Constraint,  'stannum/constraint'
@@ -7,4 +9,16 @@ module Stannum
   autoload :Contracts,   'stannum/contracts'
   autoload :Errors,      'stannum/errors'
   autoload :Struct,      'stannum/struct'
+
+  # @return [String] the absolute path to the gem directory.
+  def self.gem_path
+    pattern = /#{File::SEPARATOR}lib#{File::SEPARATOR}?\z/
+
+    __dir__.sub(pattern, '')
+  end
+
+  # @return [String] the current version of the gem.
+  def self.version
+    @version ||= Stannum::Version.to_gem_version
+  end
 end
