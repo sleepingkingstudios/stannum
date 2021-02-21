108f9470a880bc958f4d76de2fd558fb68a1f433
Rob Smith <merlin@sleepingkingstudios.com>
2021-02-21 16:11:33 -0500

    Define errors messages for Constraints::Base.

--- spec/support/examples/constraint_examples.rb
+++ spec/support/examples/constraint_examples.rb
@@ -225,7 +225,7 @@ module Spec::Support::Examples
       it { expect(actual_errors).to be nil }
     end
 
-    shared_examples 'should not match the constraint' do
+    shared_examples 'should not match the constraint' do |messages: false|
       let(:actual_status) do
         status, _ = subject.send(match_method, actual)
 
@@ -237,7 +237,33 @@ module Spec::Support::Examples
         errors
       end
       let(:wrapped_errors) do
-        (expected_errors.is_a?(Array) ? expected_errors : [expected_errors])
+        errors =
+          if expected_errors.is_a?(Array)
+            expected_errors
+          else
+            [expected_errors]
+          end
+
+        errors
+          .map do |error|
+            {
+              data:    {},
+              message: nil,
+              path:    []
+            }.merge(error)
+          end
+      end
+      let(:wrapped_messages) do
+        # :nocov:
+        errors =
+          if expected_messages.is_a?(Array)
+            expected_messages
+          else
+            [expected_messages]
+          end
+        # :nocov:
+
+        errors
           .map do |error|
             {
               data:    {},
@@ -249,7 +275,13 @@ module Spec::Support::Examples
 
       it { expect(actual_status).to be false }
 
-      it { expect(actual_errors.to_a).to match_errors wrapped_errors }
+      it { expect(actual_errors).to match_errors wrapped_errors }
+
+      if messages
+        it 'should generate the error messages' do
+          expect(actual_errors.with_messages).to match_errors wrapped_messages
+        end
+      end
     end
 
     shared_examples 'should match the type constraint' do
