270f980e869109eea5d3fba316d0c0bbea68da4a
Rob Smith <merlin@sleepingkingstudios.com>
2021-10-15 02:09:02 -0400

    Update documentation.

--- lib/stannum/messages/default_loader.rb
+++ lib/stannum/messages/default_loader.rb
@@ -6,21 +6,40 @@ require 'yaml'
 require 'stannum/messages'
 
 module Stannum::Messages
-  # @todo
+  # Loads and parses messages configuration files and merges configuration data.
   class DefaultLoader
-    # @todo
+    # @param file_paths [Array<String>] The directories from which to load the
+    #   configuration files.
+    # @param locale [String] The name of the locale for which to load
+    #   configuration.
     def initialize(file_paths:, locale: 'en')
       @file_paths = file_paths
       @locale     = locale
     end
 
-    # @todo
+    # @return [Array<String>] the directories from which to load the
+    #   configuration files.
     attr_reader :file_paths
 
-    # @todo
+    # @return [String] the name of the locale for which to load configuration.
     attr_reader :locale
 
-    # @todo
+    # Loads and parses each file, then deep merges the data from each file.
+    #
+    # The configuration file should be either a Ruby file or a YAML file, with
+    # the filename of the format locale.extname, e.g. en.rb or en-gb.yml, and
+    # located in one of the directories defined in #file_paths.
+    #
+    # The contents of each file should be either a Ruby Hash or a YAML document
+    # containing an associative array, with a single key equal to the locale.
+    # The value of the key must be a Hash or associative array, which contains
+    # the scoped messages to load.
+    #
+    # Each file is read in order and parsed into a Hash. Each hash is then deep
+    # merged in sequence, with nested hashes merged together instead of
+    # overwritten.
+    #
+    # @return [Hash<Symbol, Object>] the merged configuration data.
     def call
       file_paths.reduce({}) do |config, file_path|
         loaded = load_configuration_file(file_path)
